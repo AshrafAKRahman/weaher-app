97c19a29f4b1e252cdc4deb3cf1af21df9128b3a
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-30 11:54:03 +0000

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to your Weather App</h1>
+      <p>Get the latest weather information for your location</p>
+    </header>
+  );
+};
+
+export default Header;
